4e20682f2a57801f6736182c8d63a83bc673006c
Evgen K <eok8177@gmail.com>
2021-03-29 22:16:08 +0300

    webView

---
+++ app/src/main/java/com/example/player/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.player;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+/*
+ * Main Activity class that loads {@link MainFragment}.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = findViewById(R.id.main_browse_fragment);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setMediaPlaybackRequiresUserGesture(false);
+        webView.setFocusable(true);
+        webView.loadUrl("http://909090.me/");
+    }
+
+}
+
+
+
+
