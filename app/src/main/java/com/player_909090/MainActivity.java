33472ede6c0b0e82f6d09e774549a25739db85b3
Evgen K <eok8177@gmail.com>
2021-04-29 22:53:12 +0300

    check connection

--- app/src/main/java/com/player_909090/MainActivity.java
+++ app/src/main/java/com/player_909090/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
@@ -17,6 +18,13 @@ import android.widget.FrameLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
 /*
  * Main Activity class that loads {@link MainFragment}.
  */
@@ -28,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
         webView = findViewById(R.id.main_browse_fragment);
 
@@ -80,10 +91,12 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onReceivedError(WebView webView, WebResourceRequest request, WebResourceError error) {
-            String htmlData = "<html><body style=\"display:flex;align-items:center;justify-content:center;background:#12141a;\"><h1 style=\"color:#fff;\">Отсутствует подключение к интернету</h1></body>";
-            webView.loadUrl("about:blank");
-            webView.loadDataWithBaseURL(null,htmlData, "text/html", "UTF-8",null);
-            webView.invalidate();
+            if (!isConnectedToServer("http://909090.me/", 300)) {
+                String htmlData = "<html><body style=\"display:flex;align-items:center;justify-content:center;background:#12141a;\"><h1 style=\"color:#fff;\">Отсутствует подключение к интернету</h1></body>";
+                webView.loadUrl("about:blank");
+                webView.loadDataWithBaseURL(null,htmlData, "text/html", "UTF-8",null);
+                webView.invalidate();
+            }
         }
     }
 
@@ -128,6 +141,21 @@ public class MainActivity extends AppCompatActivity {
             this.mCustomViewCallback = paramCustomViewCallback;
             ((FrameLayout)getWindow().getDecorView()).addView(this.mCustomView, new FrameLayout.LayoutParams(-1, -1));
             getWindow().getDecorView().setSystemUiVisibility(3846 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        }
+    }
+
+    public boolean isConnectedToServer(String url, int timeout) {
+        try {
+            URL link = new URL(url);
+            HttpURLConnection urlc = (HttpURLConnection) link.openConnection();
+            urlc.setRequestProperty("User-Agent", "Android Application");
+            urlc.setRequestProperty("Connection", "close");
+            urlc.setConnectTimeout(10 * timeout);
+            urlc.connect();
+            return (urlc.getResponseCode() == 200);
+        } catch (IOException e) {
+            //Log.e(TAG, e.getMessage());
+            return false;
         }
     }
 
